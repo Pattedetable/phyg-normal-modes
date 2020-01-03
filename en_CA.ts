b6daafb584ca5286c2a0429da96cb719199602ff
Manuel Barrette <ragnathok@gmail.com>
2020-01-03 14:26:51 -0500

    Traductions / Translations

---
+++ en_CA.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en" sourcelanguage="fr">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Modes propres</source>
+        <translation>Normal modes</translation>
+    </message>
+    <message>
+        <source>Quitter</source>
+        <translation>Quit</translation>
+    </message>
+    <message>
+        <source>Fondamental</source>
+        <translation>Fundamental</translation>
+    </message>
+    <message>
+        <source>2e harmonique</source>
+        <translation>2nd harmonic</translation>
+    </message>
+    <message>
+        <source>3e harmonique</source>
+        <translation>3rd harmonic</translation>
+    </message>
+    <message>
+        <source>4e harmonique</source>
+        <translation>4th harmonic</translation>
+    </message>
+    <message>
+        <source>5e harmonique</source>
+        <translation>5th harmonic</translation>
+    </message>
+    <message>
+        <source>Amplitude</source>
+        <translation>Amplitude</translation>
+    </message>
+    <message>
+        <source>Redémarrer</source>
+        <translation>Restart</translation>
+    </message>
+    <message>
+        <source>Aide</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <source>À propos</source>
+        <translation>About</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <source>À propos</source>
+        <translation>About</translation>
+    </message>
+    <message>
+        <source>Fermer</source>
+        <translation>Close</translation>
+    </message>
+</context>
+</TS>
---
+++ fr_CA.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr" sourcelanguage="fr">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Modes propres</source>
+        <translation>Modes propres</translation>
+    </message>
+    <message>
+        <source>Quitter</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <source>Fondamental</source>
+        <translation>Fondamental</translation>
+    </message>
+    <message>
+        <source>2e harmonique</source>
+        <translation>2e harmonique</translation>
+    </message>
+    <message>
+        <source>3e harmonique</source>
+        <translation>3e harmonique</translation>
+    </message>
+    <message>
+        <source>4e harmonique</source>
+        <translation>4e harmonique</translation>
+    </message>
+    <message>
+        <source>5e harmonique</source>
+        <translation>5e harmonique</translation>
+    </message>
+    <message>
+        <source>Amplitude</source>
+        <translation>Amplitude</translation>
+    </message>
+    <message>
+        <source>Redémarrer</source>
+        <translation>Redémarrer</translation>
+    </message>
+    <message>
+        <source>Aide</source>
+        <translation>Aide</translation>
+    </message>
+    <message>
+        <source>À propos</source>
+        <translation>À propos</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <source>À propos</source>
+        <translation>À propos</translation>
+    </message>
+    <message>
+        <source>Fermer</source>
+        <translation>Fermer</translation>
+    </message>
+</context>
+</TS>
