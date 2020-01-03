d46b1590c84216817bf173333c3799f74dc8168f
Manuel Barrette <ragnathok@gmail.com>
2020-01-03 14:17:12 -0500

    First working version / Première version fonctionnelle

---
+++ modes_main_window.py
@@ -0,0 +1,343 @@
+#
+# Copyright 2020 Manuel Barrette
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+import matplotlib.animation as anim
+import os, platform
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow, Dialog, parent):
+
+        self.figure = plt.figure()
+
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1200, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget) # Quit
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.gridLayout.addWidget(self.pushButton_2, 14, 0, 1, 3)
+
+#        self.pushButton = QtWidgets.QPushButton(self.centralwidget) # Export video
+#        self.pushButton.setObjectName("pushButton")
+#        self.gridLayout.addWidget(self.pushButton, 11, 0, 1, 3)
+
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget) # Restart
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.gridLayout.addWidget(self.pushButton_3, 13, 0, 1, 3)
+
+
+        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.gridLayout.addItem(spacerItem, 12, 0, 1, 1)
+
+
+        self.radioButton = QtWidgets.QRadioButton(self.centralwidget) # Fondamental
+        self.radioButton.setObjectName("radioButton")
+        self.radioButton.setAutoExclusive(False)
+        self.gridLayout.addWidget(self.radioButton, 0, 0, 1, 3)
+
+        self.horizontalSlider = QtWidgets.QSlider(self.centralwidget)
+        self.horizontalSlider.setMinimum(0)
+        self.horizontalSlider.setMaximum(10)
+        self.horizontalSlider.setPageStep(5)
+        self.horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalSlider.setObjectName("horizontalSlider")
+        self.gridLayout.addWidget(self.horizontalSlider, 1, 0, 1, 3)
+
+        self.radioButton2 = QtWidgets.QRadioButton(self.centralwidget) # 2e harmonique
+        self.radioButton2.setObjectName("radioButton2")
+        self.radioButton2.setAutoExclusive(False)
+        self.gridLayout.addWidget(self.radioButton2, 2, 0, 1, 3)
+
+        self.horizontalSlider2 = QtWidgets.QSlider(self.centralwidget)
+        self.horizontalSlider2.setMinimum(0)
+        self.horizontalSlider2.setMaximum(10)
+        self.horizontalSlider2.setPageStep(5)
+        self.horizontalSlider2.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalSlider2.setObjectName("horizontalSlider2")
+        self.gridLayout.addWidget(self.horizontalSlider2, 3, 0, 1, 3)
+
+        self.radioButton3 = QtWidgets.QRadioButton(self.centralwidget) # 3e harmonique
+        self.radioButton3.setObjectName("radioButton3")
+        self.radioButton3.setAutoExclusive(False)
+        self.gridLayout.addWidget(self.radioButton3, 4, 0, 1, 3)
+
+        self.horizontalSlider3 = QtWidgets.QSlider(self.centralwidget)
+        self.horizontalSlider3.setMinimum(0)
+        self.horizontalSlider3.setMaximum(10)
+        self.horizontalSlider3.setPageStep(5)
+        self.horizontalSlider3.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalSlider3.setObjectName("horizontalSlider3")
+        self.gridLayout.addWidget(self.horizontalSlider3, 5, 0, 1, 3)
+
+        self.radioButton4 = QtWidgets.QRadioButton(self.centralwidget) # 4e harmonique
+        self.radioButton4.setObjectName("radioButton4")
+        self.radioButton4.setAutoExclusive(False)
+        self.gridLayout.addWidget(self.radioButton4, 6, 0, 1, 3)
+
+        self.horizontalSlider4 = QtWidgets.QSlider(self.centralwidget)
+        self.horizontalSlider4.setMinimum(0)
+        self.horizontalSlider4.setMaximum(10)
+        self.horizontalSlider4.setPageStep(5)
+        self.horizontalSlider4.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalSlider4.setObjectName("horizontalSlider4")
+        self.gridLayout.addWidget(self.horizontalSlider4, 7, 0, 1, 3)
+
+        self.radioButton5 = QtWidgets.QRadioButton(self.centralwidget) # 5e harmonique
+        self.radioButton5.setObjectName("radioButton5")
+        self.radioButton5.setAutoExclusive(False)
+        self.gridLayout.addWidget(self.radioButton5, 8, 0, 1, 3)
+
+        self.horizontalSlider5 = QtWidgets.QSlider(self.centralwidget)
+        self.horizontalSlider5.setMinimum(0)
+        self.horizontalSlider5.setMaximum(10)
+        self.horizontalSlider5.setPageStep(5)
+        self.horizontalSlider5.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalSlider5.setObjectName("horizontalSlider5")
+        self.gridLayout.addWidget(self.horizontalSlider5, 9, 0, 1, 3)
+
+
+        self.canvas = FigureCanvas(self.figure) # Graph
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.canvas.sizePolicy().hasHeightForWidth())
+        self.canvas.setSizePolicy(sizePolicy)
+        self.canvas.setObjectName("canvas")
+        self.gridLayout.addWidget(self.canvas, 0, 3, 13, 1)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1001, 25))
+        self.menubar.setObjectName("menubar")
+        self.menu_aide = QtWidgets.QMenu(self.menubar)
+        self.menu_aide.setObjectName("menu_aide")
+        MainWindow.setMenuBar(self.menubar)
+        self.action_propos = QtWidgets.QAction(MainWindow)
+        self.action_propos.setObjectName("action_propos")
+        self.menu_aide.addAction(self.action_propos)
+        self.menubar.addAction(self.menu_aide.menuAction())
+
+        # Initial parameters
+        self.horizontalSlider.setValue(10)
+        self.horizontalSlider2.setValue(0)
+        self.horizontalSlider3.setValue(0)
+        self.horizontalSlider4.setValue(0)
+        self.horizontalSlider5.setValue(0)
+        self.radioButton.toggle()
+
+
+
+        # Operating system detection
+        self.systeme_exploitation = platform.system()
+#        if self.systeme_exploitation == "Darwin":
+#            self.pushButton.setDisabled(True)
+
+        self.retranslateUi(MainWindow)
+
+        # Start animation
+        self.animationTempsReel()
+
+        # Buttons triggers
+        self.action_propos.triggered.connect(lambda: Dialog.show())
+        self.horizontalSlider.valueChanged['int'].connect(lambda: self.stopAnim())
+        self.horizontalSlider.valueChanged['int'].connect(lambda: self.animationTempsReel())
+        self.horizontalSlider.valueChanged['int'].connect(lambda: self.retranslateUi(MainWindow))
+        self.horizontalSlider2.valueChanged['int'].connect(lambda: self.stopAnim())
+        self.horizontalSlider2.valueChanged['int'].connect(lambda: self.animationTempsReel())
+        self.horizontalSlider2.valueChanged['int'].connect(lambda: self.retranslateUi(MainWindow))
+        self.horizontalSlider3.valueChanged['int'].connect(lambda: self.stopAnim())
+        self.horizontalSlider3.valueChanged['int'].connect(lambda: self.animationTempsReel())
+        self.horizontalSlider3.valueChanged['int'].connect(lambda: self.retranslateUi(MainWindow))
+        self.horizontalSlider4.valueChanged['int'].connect(lambda: self.stopAnim())
+        self.horizontalSlider4.valueChanged['int'].connect(lambda: self.animationTempsReel())
+        self.horizontalSlider4.valueChanged['int'].connect(lambda: self.retranslateUi(MainWindow))
+        self.horizontalSlider5.valueChanged['int'].connect(lambda: self.stopAnim())
+        self.horizontalSlider5.valueChanged['int'].connect(lambda: self.animationTempsReel())
+        self.horizontalSlider5.valueChanged['int'].connect(lambda: self.retranslateUi(MainWindow))
+        self.radioButton.toggled.connect(lambda: self.stopAnim())
+        self.radioButton.toggled.connect(lambda: self.animationTempsReel())
+        self.radioButton2.toggled.connect(lambda: self.stopAnim())
+        self.radioButton2.toggled.connect(lambda: self.animationTempsReel())
+        self.radioButton3.toggled.connect(lambda: self.stopAnim())
+        self.radioButton3.toggled.connect(lambda: self.animationTempsReel())
+        self.radioButton4.toggled.connect(lambda: self.stopAnim())
+        self.radioButton4.toggled.connect(lambda: self.animationTempsReel())
+        self.radioButton5.toggled.connect(lambda: self.stopAnim())
+        self.radioButton5.toggled.connect(lambda: self.animationTempsReel())
+        self.pushButton_3.clicked.connect(lambda: self.stopAnim())
+        self.pushButton_3.clicked.connect(lambda: self.animationTempsReel())
+
+
+#        self.pushButton.clicked.connect(lambda: self.stopAnim())
+#        self.pushButton.clicked.connect(lambda: self.exporterAnimation())
+        self.pushButton_2.clicked.connect(lambda: plt.close())
+        self.pushButton_2.clicked.connect(lambda: self.fermerEtAfficher(MainWindow, parent))
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        self._translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(self._translate("MainWindow", "Modes propres"))
+        self.pushButton_2.setText(self._translate("MainWindow", "Quitter"))
+        self.pushButton_3.setText(self._translate("MainWindow", "Redémarrer"))
+#        self.pushButton.setText(self._translate("MainWindow", "Exporter en vidéo (.mp4)"))
+        self.radioButton.setText(self._translate("MainWindow", "Fondamental"))
+        self.radioButton2.setText(self._translate("MainWindow", "2e harmonique"))
+        self.radioButton3.setText(self._translate("MainWindow", "3e harmonique"))
+        self.radioButton4.setText(self._translate("MainWindow", "4e harmonique"))
+        self.radioButton5.setText(self._translate("MainWindow", "5e harmonique"))
+        self.menu_aide.setTitle(self._translate("MainWindow", "Aide"))
+        self.action_propos.setText(self._translate("MainWindow", "À propos"))
+
+    def disableAll(self, boolean):
+        self.horizontalSlider.setDisabled(boolean)
+        self.horizontalSlider2.setDisabled(boolean)
+        self.horizontalSlider3.setDisabled(boolean)
+        self.horizontalSlider4.setDisabled(boolean)
+        self.horizontalSlider5.setDisabled(boolean)
+#        self.pushButton.setDisabled(boolean)
+        self.pushButton_2.setDisabled(boolean)
+        self.pushButton_3.setDisabled(boolean)
+        self.menu_aide.setDisabled(boolean)
+        self.radioButton.setDisabled(boolean)
+        self.radioButton2.setDisabled(boolean)
+        self.radioButton3.setDisabled(boolean)
+        self.radioButton5.setDisabled(boolean)
+        self.radioButton5.setDisabled(boolean)
+
+
+    def fermerEtAfficher(self, MainWindow, window_autre):
+#        if window_autre:
+#            window_autre.show()
+        app = QtWidgets.QApplication.instance()
+        app.closeAllWindows()
+
+    def stopAnim(self):
+        self.oscillation.event_source.stop()
+
+    def enregistrer(self):
+        if self.systeme_exploitation == 'Windows':
+            fichier = QtWidgets.QFileDialog.getSaveFileName(None, self._translate("MainWindow", "Enregister sous..."), os.getenv('HOMEPATH'), 'Vidéos (*.mp4)')
+        elif self.systeme_exploitation == 'Darwin' or 'Linux':
+            fichier = QtWidgets.QFileDialog.getSaveFileName(None, self._translate("MainWindow", "Enregister sous..."), os.getenv('HOME'), 'Vidéos (*.mp4)')
+        else:
+            print("Système non supporté officiellement.  Enregistrement dans le dossier de travail sous le nom 'animation.mp4'.")
+            fichier = ['animation', None]
+        return fichier[0]
+
+    def exporterAnimation(self):
+        nom_anim = self.enregistrer()
+        if nom_anim[-4:] != ".mp4":
+            nom_anim = nom_anim + ".mp4"
+        self.oscillation.save(nom_anim)
+        self.animationTempsReel()
+
+
+    def initAnimation(self):
+        """ Define parameters and setup the base graphic """
+
+        self.figure.clear()
+
+        # Important parameters
+        nb_particules = 1
+        num_frames = 45
+        longueur = 10
+        vitesse = 1
+        couleur = 'k'
+
+        grillex = np.linspace(0, longueur, 200)
+
+        self.ax2 = self.figure.add_subplot(111)
+
+
+
+#        self.ax2.set_ylabel(self._translate("MainWindow", "Position") + " (m)")
+#        self.ax2.set_xlabel(self._translate("MainWindow", "Temps") + " (s)")
+
+#        self.ax2.grid(True)
+
+        self.ax2.axis([-1, longueur+1, -10, 10])
+#        self.ax2.set_yticks([-amplitude, 0, amplitude])
+
+        deplacement = 0*grillex
+        graph2, = self.ax2.plot(grillex, deplacement, color=couleur)
+
+
+
+        return num_frames, grillex, deplacement, longueur, vitesse, graph2
+
+
+    def animationTempsReel(self):
+        """ Display the animation in real time """
+
+        [num_frames, grillex, deplacement, longueur, vitesse, graph2] = self.initAnimation()
+
+        period = 2*np.pi/(2*np.pi*vitesse/(2*longueur))
+#        num_frames = int(num_frames*period)
+
+
+        tempss = np.linspace(0, period, num_frames)
+        self.frames_particles = []
+
+        def update(i):
+            for frame in self.frames_particles:
+                frame.remove()
+            self.frames_particles = []
+            temps = tempss[i]
+            deplacement = 0
+            if self.radioButton.isChecked():
+                amplitude = self.horizontalSlider.value()/10
+                vecteur_onde = 2*np.pi/(2*longueur)
+                omega = 2*np.pi*vitesse/(2*longueur)
+                deplacement = deplacement + 2*amplitude*np.sin(vecteur_onde*grillex)*np.cos(omega*temps)
+            if self.radioButton2.isChecked():
+                amplitude = self.horizontalSlider2.value()/10
+                vecteur_onde = 2*np.pi/(longueur)
+                omega = 2*2*np.pi*vitesse/(2*longueur)
+                deplacement = deplacement + 2*amplitude*np.sin(vecteur_onde*grillex)*np.cos(omega*temps)
+            if self.radioButton3.isChecked():
+                amplitude = self.horizontalSlider3.value()/10
+                vecteur_onde = 2*np.pi/(2*longueur/3)
+                omega = 3*2*np.pi*vitesse/(2*longueur)
+                deplacement = deplacement + 2*amplitude*np.sin(vecteur_onde*grillex)*np.cos(omega*temps)
+            if self.radioButton4.isChecked():
+                amplitude = self.horizontalSlider4.value()/10
+                vecteur_onde = 2*np.pi/(longueur/2)
+                omega = 4*2*np.pi*vitesse/(2*longueur)
+                deplacement = deplacement + 2*amplitude*np.sin(vecteur_onde*grillex)*np.cos(omega*temps)
+            if self.radioButton5.isChecked():
+                amplitude = self.horizontalSlider5.value()/10
+                vecteur_onde = 2*np.pi/(2*longueur/5)
+                omega = 5*2*np.pi*vitesse/(2*longueur)
+                deplacement = deplacement + 2*amplitude*np.sin(vecteur_onde*grillex)*np.cos(omega*temps)
+            graph2.set_ydata(deplacement)
+        self.oscillation = anim.FuncAnimation(self.figure, update, frames=num_frames, repeat=True, interval=period/num_frames)
+        self.canvas.draw()
